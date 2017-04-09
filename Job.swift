732f1c2b7e53d1b6970eb810c3a4d2184266391c
YukkeeChang <yukkeechang@gmail.com>
2017-04-08 20:22:51 -0700

    saved posts and pictures from first jobpost vc

---
+++ Job.swift
@@ -0,0 +1,17 @@
+//
+//  Job.swift
+//  weWrk_1
+//
+//  Created by Yukkee chang on 4/8/17.
+//  Copyright © 2017 luis castill0. All rights reserved.
+//
+
+import Foundation
+
+class Job: NSObject {
+    
+    var title: String?
+    var company: String?
+    var location: String?
+    
+}
