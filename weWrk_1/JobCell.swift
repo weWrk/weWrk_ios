ff670987e0d10262e23caf10cf92c768201e2044
cpguan <pguan000@gmail.com>
2017-04-27 14:26:45 -0400

    rebase to JobPost

---
+++ weWrk_1/JobCell.swift
@@ -0,0 +1,51 @@
+//
+//  JobCell.swift
+//  weWrk_1
+//
+//  Created by Pan Guan on 4/16/17.
+//  Copyright © 2017 luis castill0. All rights reserved.
+//
+
+import UIKit
+
+class JobCell: UITableViewCell {
+
+    @IBOutlet weak var JobAddress: UILabel!
+    
+    @IBOutlet weak var siteDescription: UILabel!
+    
+    @IBOutlet fileprivate weak var collectionView: UICollectionView!
+    
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
+
+extension JobCell {
+
+    func setCollectionViewDataSourceDelegate
+    <D: UICollectionViewDataSource & UICollectionViewDelegate>
+    (dataSourceDelegate: D, forRow row: Int) {
+    
+    collectionView.delegate = dataSourceDelegate
+    collectionView.dataSource = dataSourceDelegate
+    collectionView.tag = row
+    collectionView.setContentOffset(collectionView.contentOffset, animated:false) // Stops collection view if it was scrolling.
+    
+    collectionView.reloadData()
+    }
+    
+    var collectionViewOffset: CGFloat {
+        set { collectionView.contentOffset.x = newValue }
+        get { return collectionView.contentOffset.x }
+    }
+}
---
+++ weWrk_1/getPostImageByPostKey.swift
@@ -0,0 +1,50 @@
+//
+//  getPostImageByPostKey.swift
+//  weWrk_1
+//
+//  Created by Pan Guan on 4/21/17.
+//  Copyright © 2017 luis castill0. All rights reserved.
+//
+
+import Foundation
+import UIKit
+import Firebase
+
+func getPostImageByPostKey(IDKey: String) -> [siteImagePost]{
+    print("IDkey1: \(IDKey)")
+    var countNum = 0
+    var imagePosts:[siteImagePost] = []
+    
+    ProfileDataService.ds.REF_SITE_IMAGE_POSTS.observe(.value, with: { (snapshot) in
+        
+        if let snapshot = snapshot.children.allObjects as? [FIRDataSnapshot] {
+            for snap in snapshot {
+                print("SNAP: \(snap)")
+                
+                if let postDict = snap.value as? Dictionary<String, AnyObject> {
+                    let key = snap.key
+                    
+                    let sitePostKey = snap.childSnapshot(forPath:"sitePostKey").value as? String
+                    print("sitePostKey: \(sitePostKey)")
+                    
+                    let imageURL = snap.childSnapshot(forPath:"imageURL").value as? String
+                    
+                    if sitePostKey == IDKey {
+                        countNum = countNum + 1
+                        print("countNum: \(countNum)")
+                        let imagePost = siteImagePost(postKey: key, postData: postDict)
+                        print("imagePost: \(imagePost)")
+                        imagePosts.append(imagePost)
+                    }
+                
+                }
+            }
+            
+        }
+        print("imagePosts5: \(imagePosts.count)")
+ //       return imagePosts
+        
+    })
+    print("imagePosts5: \(imagePosts.count)")
+    return imagePosts
+}
